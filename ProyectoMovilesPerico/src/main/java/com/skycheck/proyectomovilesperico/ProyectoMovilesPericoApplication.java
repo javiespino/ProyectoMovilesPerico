5f4fb916088dde396043e20c7e74e6d547325466
javiespino <jespinot05@gmail.com>
2026-02-24 10:33:54 +0100

    Git inicial

---
+++ ProyectoMovilesPerico/src/main/java/com/skycheck/proyectomovilesperico/ProyectoMovilesPericoApplication.java
@@ -0,0 +1,13 @@
+package com.skycheck.proyectomovilesperico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMovilesPericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMovilesPericoApplication.class, args);
+	}
+
+}
---
+++ ProyectoMovilesPerico/src/test/java/com/skycheck/proyectomovilesperico/ProyectoMovilesPericoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skycheck.proyectomovilesperico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMovilesPericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
